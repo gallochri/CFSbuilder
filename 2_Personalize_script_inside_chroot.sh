31ac4e83cb0f989ea04800df70b4616d78da3ffd
gallochri <chri@gallochri.com>
2015-07-22 19:06:26 +0200

    Non free firmware added

--- 2_Personalize_script_inside_chroot.sh
+++ 2_Personalize_script_inside_chroot.sh
@@ -2,7 +2,7 @@
 
 # Aggiunta repository standard
 rm -r /etc/apt/sources.list
-echo "deb http://archive.raspbian.org/raspbian wheezy main" >> /etc/apt/sources.list
+echo "deb http://archive.raspbian.org/raspbian wheezy main non-free" >> /etc/apt/sources.list
 echo "deb http://archive.raspberrypi.org/debian/ wheezy main" >> /etc/apt/sources.list
 wget http://archive.raspbian.org/raspbian.public.key -O - | apt-key add -
 wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key -O - | apt-key add -
@@ -59,16 +59,16 @@ adduser --gecos "" pi
 echo "##########Installazione pacchetti base##########"
 apt-get install -y bash-completion blt build-essential
 apt-get install -y cgroup-bin curl
-apt-get install -y debconf-utils debian-reference-common debian-reference-en dhcpcd5 dillo dphys-swapfile
+apt-get install -y debconf-utils debian-reference-common debian-reference-it dhcpcd5 dphys-swapfile
 apt-get install -y ed esound-common
 apt-get install -y fake-hwclock fbset fonts-freefont-ttf fonts-roboto
-apt-get install -y galculator gdb gdbserver gettext-base gir1.2-glib-2.0 git git-core git-man gksu gnome-icon-theme-symbolic gpicview gvfs-backends gvfs-fuse
+apt-get install -y gdb gdbserver gettext-base gir1.2-glib-2.0 git git-core git-man gksu gnome-icon-theme-symbolic gvfs-backends gvfs-fuse
 apt-get install -y hardlink
 apt-get install -y idle idle-python2.7 idle-python3.2 idle3 python-tk ifplugd i2c-tools
 apt-get install -y jackd jackd2 
 apt-get install -y manpages-dev mawk menu menu-xdg
-apt-get install -y ncdu netsurf-common netsurf-gtk nfs-common nuscratch                                         
-apt-get install -y obconf omxplayer openbox openresolv openssl oracle-java8-jdk
+apt-get install -y ncdu nfs-common nuscratch                                         
+apt-get install -y omxplayer openresolv openssl oracle-java8-jdk
 apt-get install -y pkg-config poppler-data poppler-utils pypy-setuptools pypy-upstream pypy-upstream-dev pypy-upstream-doc python-numpy 
 apt-get install -y python-picamera python-pifacecommon python-pifacedigitalio python-pygame python-rpi.gpio python-serial python3-numpy 
 apt-get install -y python3-picamera python3-pifacecommon python3-pifacedigital-scratch-handler python3-pifacedigitalio python3-pygame python3-rpi.gpio python3-serial 
@@ -78,9 +78,12 @@ apt-get install -y timidity timidity-daemon
 apt-get install -y usbutils
 apt-get install -y v4l-utils vim
 apt-get install -y wireless-tools wpagui wpasupplicant
-apt-get install -y xpdf xserver-xorg-video-fbturbo x2x xarchiver xfconf xinit xserver-xorg-video-fbdev
+apt-get install -y xserver-xorg-video-fbturbo x2x xinit xserver-xorg-video-fbdev
 
-#apt-get install -y firmware-atheros firmware-libertas firmware-ralink firmware-realtek
+echo "##########Installazione pacchetti non-free##########"
+apt-get install -y firmware-atheros firmware-libertas firmware-ralink firmware-realtek
+
+#disponibile per Jessie
 #apt-get install -y weston
 
 echo "################Installazione mate################"
@@ -106,6 +109,7 @@ sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/"
 sed /etc/lightdm/lightdm.conf -i -e "s/^# user-session =.*/user-session=mate-session/"
 sed /etc/lightdm/lightdm.conf -i -e "s/^#user-session=.*/user-session=mate-session/"
 cp -r /root/config/home/pi/xinitrc	/home/pi/.xinitrc
+update-alternatives --set x-session-manager /usr/bin/mate-session
 
 # Configurazione usbmount.conf
 sed -i -e 's/""/"-fstype=vfat,flush,gid=plugdev,dmask=0007,fmask=0117"/g' /etc/usbmount/usbmount.conf
@@ -122,10 +126,7 @@ invoke-rc.d keyboard-setup start
 # Configurazione timezone
 dpkg-reconfigure tzdata
 
-# Aggiornamento kernel
-rpi-update
-
 # Pulizia
 apt-get clean
 
-echo "Ora esci dal chroot digitando exit"
+echo "Ora lancia rpi-update e poi esci dal chroot digitando exit"
