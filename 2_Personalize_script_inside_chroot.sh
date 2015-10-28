071dead645fe162b724f5421172df5a28d8da255
gallochri <chri@gallochri.com>
2015-10-28 22:50:16 +0100

    Personalize scritp revision till R-X packages

--- 2_Personalize_script_inside_chroot.sh
+++ 2_Personalize_script_inside_chroot.sh
@@ -2,7 +2,7 @@
 
 echo "###############Creazione utente pi###############"
 adduser --gecos "" pi
-# --disable-password
+# --disabled-password
 # TODO Mettendo l'opzione --disabled-password si crea un utente senza password.
 # TODO L'utente così creato non fa il login in X, probabile ci sia da configurare X
 # TODO in modo da permettere il login senza password.
@@ -17,11 +17,9 @@ echo "deb http://archive.raspbian.org/mate jessie main" >> /etc/apt/sources.list
 wget http://archive.raspbian.org/raspbian.public.key -O - | apt-key add -
 wget http://archive.raspberrypi.org/debian/raspberrypi.gpg.key -O - | apt-key add -
 
-# Copiatura configurazione eth0 con DHCP e wpa supplicant
-cp -r /root/config/etc/network/interfaces /etc/network/interfaces
-cp -r /root/config/etc/default/ifplugd /etc/default/ifplugd
-mkdir /etc/wpa_supplicant/
-cp -r /root/config/etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/
+# Hostname
+sh -c 'echo raspberrypi >/etc/hostname'
+sh -c 'echo 127.0.0.1	raspberrypi >>/etc/hosts'
 
 # Repair filesystem during boot
 cp -r /root/config/etc/default/rcS /etc/default/rcS
@@ -44,34 +42,57 @@ cp -r /root/config/root/profile /root/
 apt-get update
 
 echo "##########Installazione sistema di base##########"
-apt-get install -y locales sudo openssh-server ntp patch less rsync raspi-config usbmount
 
+apt-get install -y locales
+# Configurazione Locale
+# en_GB.UTF-8
+# it_IT.UTF_8
+dpkg-reconfigure locales
+
+apt-get install -y sudo
 # configurazione Sudo
 cp -r /root/config/etc/sudoers /etc/
 
+apt-get install -y ntp
+# Configurazione timezone
+dpkg-reconfigure tzdata
+
+apt-get install -y console-setup
+# Configurazione della tastiera
+
+apt-get install -y ifplugd wpasupplicant
+# Copiatura configurazione eth0 con DHCP e wpa supplicant
+cp -r /root/config/etc/network/interfaces /etc/network/interfaces
+cp -r /root/config/etc/default/ifplugd /etc/default/ifplugd
+mkdir /etc/wpa_supplicant/
+cp -r /root/config/etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/
+
+apt-get install -y openssh-server patch rsync raspi-config usbmount
+
 echo "##########Installazione pacchetti base###########"
 echo "##########Pacchetti B-E###########"
-apt-get install -y bash-completion binutils blt build-essential bzip2
-apt-get install -y ca-certificates console-setup console-setup-linux cryptsetup-bin cups-bsd cups-client cups-common curl
-apt-get install -y dbus-x11 dc dconf-gsettings-backend:armhf debconf-utils debian-reference-common debian-reference-it dhcpcd5 dphys-swapfile
+apt-get install -y bash-completion binutils blt build-essential
+apt-get install -y ca-certificates cryptsetup-bin cups-bsd curl
+apt-get install -y dbus-x11 dc dconf-gsettings-backend debconf-utils dhcpcd5
 apt-get install -y ed eject esound-common
 echo "##########Pacchetti F-M###########"
-apt-get install -y fake-hwclock fbset fontconfig fonts-dejavu fonts-dejavu-extra fonts-freefont-ttf fonts-opensymbol fonts-sil-gentium-basic fonts-roboto fuse
-apt-get install -y gdb gdbserver gettext-base git git-core git-man gksu gvfs-backends gvfs-fuse
-apt-get install -y idle idle-python2.7 idle-python3.4 idle3 ifplugd
-apt-get install -y jackd jackd2 java-common javascript-common
-apt-get install -y ncdu nfs-common nuscratch
+apt-get install -y fake-hwclock fbset fuse
+apt-get install -y fontconfig fonts-dejavu fonts-freefont-ttf fonts-opensymbol fonts-sil-gentium-basic fonts-roboto
+apt-get install -y gdb gettext-base git git-core gksu gvfs-backends gvfs-fuse
+apt-get install -y idle idle3
+apt-get install -y java-common javascript-common
+apt-get install -y nfs-common nuscratch
 echo "##########Pacchetti O-P###########"
 apt-get install -y omxplayer oracle-java8-jdk
-apt-get install -y poppler-utils pypy-setuptools pypy-upstream pypy-upstream-dev pypy-upstream-doc
-apt-get install -y python-picamera python-pifacecommon python-pifacedigitalio python-pygame python-rpi.gpio python-serial python3-numpy
-apt-get install -y python3-picamera python3-pifacecommon python3-pifacedigital-scratch-handler python3-pifacedigitalio python3-pygame
-apt-get install -y python3-rpi.gpio python3-serial
+apt-get install -y poppler-utils pypy-setuptools
+apt-get install -y python-picamera python-pifacecommon python-pifacedigitalio python-pygame python-rpi.gpio python-serial
+apt-get install -y python3-numpy python3-picamera python3-pifacecommon python3-pifacedigital-scratch-handler
+apt-get install -y python3-pygame python3-rpi.gpio python3-serial
 echo "##########Pacchetti R-X###########"
 apt-get install -y raspberrypi-artwork raspberrypi-net-mods raspi-gpio rpi-update
 apt-get install -y smartsim sonic-pi ssh strace timidity usbutils v4l-utils vim
-apt-get install -y wireless-tools wpagui wpasupplicant wiringpi
-apt-get install -y xserver-xorg-video-fbturbo x2x xinit xserver-xorg-video-fbdev
+apt-get install -y wireless-tools wpagui
+apt-get install -y xserver-xorg-video-fbturbo x2x xinit xserver-xorg-video-fbdev x11-xserver-utils
 
 echo "#########Installazione pacchetti non-free##########"
 apt-get install -y firmware-atheros firmware-brcm80211 firmware-libertas firmware-ralink firmware-realtek
@@ -93,8 +114,7 @@ echo "#############Registrazione CFS####################"
 #TODO con Jessie Debian è passata definitivamente a Systemd
 #TODO Aggiornare gli script!
 echo "#############Generatore di Hostname###############"
-sh -c 'echo raspberrypi >/etc/hostname'
-sh -c 'echo 127.0.0.1	raspberrypi >>/etc/hosts'
+
 #install -m 755 /root/sources/name_generator /usr/local/bin/
 #install -m 755 /root/sources/hostname.sh /etc/init.d/
 #install -m 755 /root/sources/hostname_changed.sh /etc/init.d/
@@ -127,12 +147,7 @@ sed /etc/lightdm/lightdm.conf -i -e "s/^#autologin-user=.*/autologin-user=pi/"
 # Configurazione usbmount.conf
 sed -i -e 's/""/"-fstype=vfat,flush,gid=plugdev,dmask=0007,fmask=0117"/g' /etc/usbmount/usbmount.conf
 
-# Configurazione Locale
-# installare anche en_GB.UTF-8
-dpkg-reconfigure locales
 
-# Configurazione timezone
-dpkg-reconfigure tzdata
 
 echo "##############Install scratch GPIO6################"
 cd /home/pi
